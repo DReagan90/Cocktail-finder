fd058dba29603a54d3425674315f5cb57c8cd5a5
ReaganBaimet0 <reagan.baimet@student.moringaschool.com>
2025-03-29 00:45:40 +0300

    DOMelements

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
- 
+const searchInput = document.getElementById('search-input');
+const searchBtn = document.getElementById('search-btn');
+const randomBtn = document.getElementById('random-btn');
+const modeToggle = document.getElementById('mode-toggle');
+const categoryFilter = document.getElementById('category-filter');
+const cocktailContainer = document.getElementById('cocktail-container');
+const body = document.body;
